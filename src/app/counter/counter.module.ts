0ec59b1f9867938e5f5aed42860c24b650b2e421
Salvador Menco <salvador.menco@ias.com.co>
2023-10-03 17:51:02 -0500

    Fin de seccion 4

---
+++ src/app/counter/counter.module.ts
@@ -0,0 +1,12 @@
+import { CounterComponent } from './components/counter/counter.component';
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    declarations: [
+      CounterComponent
+    ],
+    exports:[
+      CounterComponent
+    ]
+})
+export class CounterModule{}
